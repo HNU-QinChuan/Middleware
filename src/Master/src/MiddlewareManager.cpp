097aba3801f2258dafd7d03d2326481da70b22a7
yc <1422776724@qq.com>
2025-02-14 18:52:18 +0800

    test

--- src/Master/src/MiddlewareManager.cpp
+++ src/Master/src/MiddlewareManager.cpp
@@ -12,6 +12,7 @@ namespace Hnu::Middleware {
   bool MiddlewareManager::addNode(const std::string& name) {
     if (getInstance().m_nodes.contains(name)) {
       return false;
+
     }
     getInstance().m_nodes[name]=std::make_shared<Node>(name);
   }
