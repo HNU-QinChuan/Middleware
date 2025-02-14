4bb52aeabae44424e464df4870494d984701aa25
xuyiaoao <xuyiaoao@gmail.com>
2025-02-14 18:54:06 +0800

    aoao

--- src/Master/src/MiddlewareManager.cpp
+++ src/Master/src/MiddlewareManager.cpp
@@ -12,7 +12,6 @@ namespace Hnu::Middleware {
   bool MiddlewareManager::addNode(const std::string& name) {
     if (getInstance().m_nodes.contains(name)) {
       return false;
-
     }
     getInstance().m_nodes[name]=std::make_shared<Node>(name);
   }
