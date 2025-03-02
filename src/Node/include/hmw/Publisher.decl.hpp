6f13c57a9df7ac0371383e5ef8ea65853d5a4da1
yc <1422776724@qq.com>
2025-03-02 19:59:18 +0800

    添加define.hpp

--- src/Node/include/hmw/Publisher.decl.hpp
+++ src/Node/include/hmw/Publisher.decl.hpp
@@ -6,28 +6,12 @@
 
 
 
-#include<string>
-#include<boost/asio.hpp>
-#include<boost/lockfree/spsc_queue.hpp>
-#include<boost/beast.hpp>
-#include<boost/interprocess/containers/string.hpp>
-#include<boost/interprocess/managed_shared_memory.hpp>
+#include "Define.hpp"
 #include "hmw/PublisherInterface.hpp"
 
 
 namespace Hnu::Middleware {
   class Node;
-  namespace asio = boost::asio;
-  namespace beast = boost::beast;
-  namespace lockfree = boost::lockfree;
-  namespace interprocess = boost::interprocess;
-
-  constexpr std::size_t QUEUE_SIZE=10;
-
-  using char_allocator = interprocess::allocator<char, interprocess::managed_shared_memory::segment_manager>;
-  using string = interprocess::basic_string<char, std::char_traits<char>, char_allocator>;
-  using lock_free_queue=lockfree::spsc_queue<string,lockfree::capacity<QUEUE_SIZE>>;
-  using local_stream=beast::basic_stream<asio::local::stream_protocol>;
 
   template<typename Message>
   class Publisher :public PublisherInterface {
