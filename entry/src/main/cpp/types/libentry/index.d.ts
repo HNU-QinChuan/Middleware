aa977995c1d0e80eeb9b2fd2a01fccc979aa8967
likeTiii <128377143+likeTiii@users.noreply.github.com>
2025-11-06 17:40:03 +0800

    Delete entry directory

--- entry/src/main/cpp/types/libentry/index.d.ts
+++
@@ -1,6 +0,0 @@
-export const runCommand: (input: string, recordPath: string, index: number) => string;
-export const stopCommand: (command: string) => string;
-export const registerMessageCallback: ( cb: (message: string) => void) => void;
-
-
-
